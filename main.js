da95ae13b714729371aef9b4f58c47168ec7612b
LAKSHYA SINGH CHAUHAN <85747258+lakshyagithub@users.noreply.github.com>
2022-06-21 19:20:09 +0530

    Update incompleate

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-status1 = false;
+Status = false;
 
 function setup() {
   createCanvas(400, 400);
@@ -9,16 +9,26 @@ function setup() {
 function draw() {
   background(220);
   image(video, 0, 0, 400, 400);
+  if (Status == true) {
+  objectDetector.detect(video, gotResults);
+  }
 }
 
 function start() {
-  objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
 }
 
 function modelLoaded() {
   console.log("Model Loaded!");
-  status1 = true;
+  Status = true;
   video.loop();
   video.speed(1);
-  video.volume(1);
+  video.volume(0);
+}
+
+function gotResults(error, results) {
+  if (error) {
+    console.error(error);
+  }
+  console.log(results);
 }
