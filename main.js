3c724c3ad97e6f361fbc63e68e0ba59a1dae92bf
LAKSHYA SINGH CHAUHAN <85747258+lakshyagithub@users.noreply.github.com>
2022-06-21 19:51:49 +0530

    message

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 Status = false;
+object = [];
+video = ""; 
 
 function setup() {
   createCanvas(400, 400);
@@ -10,8 +12,19 @@ function draw() {
   background(220);
   image(video, 0, 0, 400, 400);
   if (Status == true) {
-  objectDetector.detect(video, gotResults);
+    objectDetector.detect(video, gotResults);
+    for (i = 0; i < object.length; i++) {
+      document.getElementById("status").innerHTML = "Object Detected";
+      document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : " + object.length;
+
+      fill(255, 0, 0);
+      percent = floor(object[i].confidence * 100);
+      text(object[i].label + " " + percent + "%", object[i].x + 15, object[i].y + 15);
+      noFill();
+      stroke(255, 0, 0);
+      rect(object[i].x, object[i].y, object[i].width, object[i].height);
   }
+}
 }
 
 function start() {
@@ -31,4 +44,5 @@ function gotResults(error, results) {
     console.error(error);
   }
   console.log(results);
+  object = results;
 }
